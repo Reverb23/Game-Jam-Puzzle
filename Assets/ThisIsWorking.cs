99175ab9ca29872537b14a2c53a7facebfd3050b
Jamie W <163033606+Reverb23@users.noreply.github.com>
2026-03-03 12:04:59 +0000

    Update ThisIsWorking.cs

--- Assets/ThisIsWorking.cs
+++ Assets/ThisIsWorking.cs
@@ -5,7 +5,7 @@ public class ThisIsWorking : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("LOLOLOLOLOLOL!");
     }
 
     // Update is called once per frame
